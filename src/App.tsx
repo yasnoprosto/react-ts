29f49d736c8d0b345abb6ebabd85c5b1837114d8
Denis Pochinskiy <ircxenon1@gmail.com>
2023-04-28 22:45:52 +0500

    2804 upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  return <div>This is App component</div>;
+}
+
+function Rating() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
     </div>
   );
+}
+
+function hello() {
+  alert("hello");
+}
+hello();
+
+function Accordion() {
+  return (
+    <div>
+      <h3>Menu</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App;
